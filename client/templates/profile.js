2d90ac6be60f349454409db83a5883fbfbd9965a
David Kezi <davidkezi6@gmail.com>
2016-05-29 21:22:47 +0300

    function to see if file is availble

--- client/templates/profile.js
+++ client/templates/profile.js
@@ -1,8 +1,16 @@
 Template.profile.events({
     "submit .editProfile": function(event){
-
         var file =  $('#profileImage').get(0).file[0];
         console.log(file);
         return false;
     }
+    if(file){
+    fsFile = new FS.File(file)
+    ProfileImages.insert(fsFile, function(err, result){
+
+    if(err){
+    throw new Meteor.Error(err);
+}
+})
+}
 });
