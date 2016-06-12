213f008c4d8cab2e6b8253d870844b422212795d
David Kezi <davidkezi6@gmail.com>
2016-06-12 23:37:19 +0300

    edited syntax

--- client/templates/profile.js
+++ client/templates/profile.js
@@ -1,24 +1,24 @@
 Template.profile.events({
     "submit .editProfile": function(event){
         var file =  $('#profileImage').get(0).file[0];
-        console.log(file);
-        return false;
-    }
+        
+
         if(file){
             fsFile = new FS.File(file)
-
 
             ProfileImages.insert(fsFile, function(err, result){
                 if(err){
                     throw new Meteor.Error(err);
                 }else{
-                    var imageLoc = 'cfs/files/';
+                    var imageLoc = 'cfs/files/ProfileImages/';
 
                     UserImages.insert({
                         userId: Meteor.userId(),
                         username: Meteor.user().username;
-                    })
-       }
-});
-}
+                    });
+                }
+            });
+        }
+        return false;
+    }    
 });
