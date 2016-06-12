eacee2e6c21d9500222614178333f58d444e29fc
David Kezi <davidkezi6@gmail.com>
2016-06-12 23:32:05 +0300

    added insert function in profile.js

--- client/templates/profile.js
+++ client/templates/profile.js
@@ -4,15 +4,20 @@ Template.profile.events({
         console.log(file);
         return false;
     }
-    if(file){
-    fsFile = new FS.File(file)
+        if(file){
+            fsFile = new FS.File(file)
 
 
-    ProfileImages.insert(fsFile, function(err, result){
-    if(err){
-    throw new Meteor.Error(err);
-    }else{
-    var imageLoc = 'cfs/files/'
+            ProfileImages.insert(fsFile, function(err, result){
+                if(err){
+                    throw new Meteor.Error(err);
+                }else{
+                    var imageLoc = 'cfs/files/';
+
+                    UserImages.insert({
+                        userId: Meteor.userId(),
+                        username: Meteor.user().username;
+                    })
        }
 });
 }
