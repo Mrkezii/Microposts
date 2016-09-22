1e2f2335ddc0ee753a2b661dfa7274f08b0e600f
David Kezi <davidkezi6@gmail.com>
2016-09-22 22:08:41 +0100

    refactor navbar, layout and posts

--- client/templates/profile.js
+++ client/templates/profile.js
@@ -1,24 +1,24 @@
 Template.profile.events({
-    "submit .editProfile": function(event){
-        var file =  $('#profileImage').get(0).file[0];
-        
+    "submit .editProfile": function (event) {
+        var file = $('#profileImage').get(0).file[0];
 
-        if(file){
+
+        if (file) {
             fsFile = new FS.File(file)
 
-            ProfileImages.insert(fsFile, function(err, result){
-                if(err){
+            ProfileImages.insert(fsFile, function (err, result) {
+                if (err) {
                     throw new Meteor.Error(err);
-                }else{
+                } else {
                     var imageLoc = 'cfs/files/ProfileImages/';
 
                     UserImages.insert({
                         userId: Meteor.userId(),
-                        username: Meteor.user().username;
+                        // username: Meteor.user().username;
                     });
                 }
             });
         }
         return false;
-    }    
+    }
 });
